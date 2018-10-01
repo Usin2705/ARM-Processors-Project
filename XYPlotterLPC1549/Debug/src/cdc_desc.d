904179a6babaa22c1441a2978f486a93e7a2e235
Nhan Phan - Laptop <Usin2705@yahoo.com>
2018-10-01 13:05:54 +0300

    docs: refactor

---
+++ XYPlotterLPC1549/Debug/src/cdc_desc.d
@@ -0,0 +1,46 @@
+src/cdc_desc.o src/cdc_desc.d: ../src/cdc_desc.c \
+ C:\Users\Usin\Desktop\Projects\ARM-Processors-Project\Lab10_03_timer_command\inc/app_usbd_cfg.h \
+ C:\Users\Usin\Desktop\Projects\ARM-Processors-Project\lpc_chip_15xx\inc/lpc_types.h \
+ C:\Users\Usin\Desktop\Projects\ARM-Processors-Project\lpc_chip_15xx\inc/error.h \
+ C:\Users\Usin\Desktop\Projects\ARM-Processors-Project\lpc_chip_15xx\inc\usbd/usbd_rom_api.h \
+ C:\Users\Usin\Desktop\Projects\ARM-Processors-Project\lpc_chip_15xx\inc\usbd/usbd.h \
+ C:\Users\Usin\Desktop\Projects\ARM-Processors-Project\lpc_chip_15xx\inc\usbd/usbd_hw.h \
+ C:\Users\Usin\Desktop\Projects\ARM-Processors-Project\lpc_chip_15xx\inc\usbd/usbd_core.h \
+ C:\Users\Usin\Desktop\Projects\ARM-Processors-Project\lpc_chip_15xx\inc\usbd/usbd_mscuser.h \
+ C:\Users\Usin\Desktop\Projects\ARM-Processors-Project\lpc_chip_15xx\inc\usbd/usbd_msc.h \
+ C:\Users\Usin\Desktop\Projects\ARM-Processors-Project\lpc_chip_15xx\inc\usbd/usbd_dfuuser.h \
+ C:\Users\Usin\Desktop\Projects\ARM-Processors-Project\lpc_chip_15xx\inc\usbd/usbd_dfu.h \
+ C:\Users\Usin\Desktop\Projects\ARM-Processors-Project\lpc_chip_15xx\inc\usbd/usbd_hiduser.h \
+ C:\Users\Usin\Desktop\Projects\ARM-Processors-Project\lpc_chip_15xx\inc\usbd/usbd_hid.h \
+ C:\Users\Usin\Desktop\Projects\ARM-Processors-Project\lpc_chip_15xx\inc\usbd/usbd_cdcuser.h \
+ C:\Users\Usin\Desktop\Projects\ARM-Processors-Project\lpc_chip_15xx\inc\usbd/usbd_cdc.h
+
+C:\Users\Usin\Desktop\Projects\ARM-Processors-Project\Lab10_03_timer_command\inc/app_usbd_cfg.h:
+
+C:\Users\Usin\Desktop\Projects\ARM-Processors-Project\lpc_chip_15xx\inc/lpc_types.h:
+
+C:\Users\Usin\Desktop\Projects\ARM-Processors-Project\lpc_chip_15xx\inc/error.h:
+
+C:\Users\Usin\Desktop\Projects\ARM-Processors-Project\lpc_chip_15xx\inc\usbd/usbd_rom_api.h:
+
+C:\Users\Usin\Desktop\Projects\ARM-Processors-Project\lpc_chip_15xx\inc\usbd/usbd.h:
+
+C:\Users\Usin\Desktop\Projects\ARM-Processors-Project\lpc_chip_15xx\inc\usbd/usbd_hw.h:
+
+C:\Users\Usin\Desktop\Projects\ARM-Processors-Project\lpc_chip_15xx\inc\usbd/usbd_core.h:
+
+C:\Users\Usin\Desktop\Projects\ARM-Processors-Project\lpc_chip_15xx\inc\usbd/usbd_mscuser.h:
+
+C:\Users\Usin\Desktop\Projects\ARM-Processors-Project\lpc_chip_15xx\inc\usbd/usbd_msc.h:
+
+C:\Users\Usin\Desktop\Projects\ARM-Processors-Project\lpc_chip_15xx\inc\usbd/usbd_dfuuser.h:
+
+C:\Users\Usin\Desktop\Projects\ARM-Processors-Project\lpc_chip_15xx\inc\usbd/usbd_dfu.h:
+
+C:\Users\Usin\Desktop\Projects\ARM-Processors-Project\lpc_chip_15xx\inc\usbd/usbd_hiduser.h:
+
+C:\Users\Usin\Desktop\Projects\ARM-Processors-Project\lpc_chip_15xx\inc\usbd/usbd_hid.h:
+
+C:\Users\Usin\Desktop\Projects\ARM-Processors-Project\lpc_chip_15xx\inc\usbd/usbd_cdcuser.h:
+
+C:\Users\Usin\Desktop\Projects\ARM-Processors-Project\lpc_chip_15xx\inc\usbd/usbd_cdc.h:
